33bcdf1f3abe73fbd58527b18dc868f075bdb8e3
swzhou <swing.zhou@hotmail.com>
2014-08-31 11:11:01 +0800

    [sw] fix ut

--- test/spec/directives/chartjs.js
+++ test/spec/directives/chartjs.js
@@ -2,19 +2,19 @@
 
 describe('Directive: chartjs', function () {
 
-  // load the directive's module
-  beforeEach(module('vSeeApp'));
+    // load the directive's module
+    beforeEach(module('vSeeApp'));
 
-  var element,
-    scope;
+    var element,
+        scope;
 
-  beforeEach(inject(function ($rootScope) {
-    scope = $rootScope.$new();
-  }));
+    beforeEach(inject(function ($rootScope) {
+        scope = $rootScope.$new();
+    }));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<chartjs></chartjs>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the chartjs directive');
-  }));
+    it('should show canvas', inject(function ($compile) {
+        element = angular.element('<div><chartjs></chartjs></div>');
+        element = $compile(element)(scope);
+        expect(element.html()).toContain('<canvas');
+    }));
 });
